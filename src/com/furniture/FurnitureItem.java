cb24833f590b78680d4ca67fba6e6966a97e726f
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-16 15:31:21 +0530

    declared no argument constructor

--- src/com/furniture/FurnitureItem.java
+++ src/com/furniture/FurnitureItem.java
@@ -6,7 +6,13 @@ public class FurnitureItem {
     private String gradeOfFurniture;
     private String furnitureUsage;
     private double price;
-
     final static int DISCOUNT = 5;
 
+    FurnitureItem(){
+        furnitureCode = 0;
+        furnitureType = null;
+        gradeOfFurniture = null;
+        furnitureUsage = null;
+        price = 0.0;
+    }
 }
