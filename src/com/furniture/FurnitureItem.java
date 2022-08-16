b6887f92d4e271e59b0c9507f8ff3342e8f46da4
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-16 15:49:13 +0530

    declared setter and getter methods

--- src/com/furniture/FurnitureItem.java
+++ src/com/furniture/FurnitureItem.java
@@ -15,4 +15,34 @@ public class FurnitureItem {
         furnitureUsage = null;
         price = 0.0;
     }
+    public void setFurnitureCode(int furnitureCode) {
+        this.furnitureCode = furnitureCode;
+    }
+    public void setFurnitureType(String furnitureType) {
+        this.furnitureType = furnitureType;
+    }
+    public void setGradeOfFurniture(String gradeOfFurniture) {
+        this.gradeOfFurniture = gradeOfFurniture;
+    }
+    public void setFurnitureUsage(String furnitureUsage) {
+        this.furnitureUsage = furnitureUsage;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getFurnitureCode() {
+        return furnitureCode;
+    }
+    public String getFurnitureType() {
+        return furnitureType;
+    }
+    public String getGradeOfFurniture() {
+        return gradeOfFurniture;
+    }
+    public String getFurnitureUsage() {
+        return furnitureUsage;
+    }
+    public double getPrice() {
+        return price;
+    }
 }
