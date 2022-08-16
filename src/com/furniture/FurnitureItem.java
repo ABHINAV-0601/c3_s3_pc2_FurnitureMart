5eab430bff00ca2a99a208ff1edd5d05ccc725e5
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-16 15:27:43 +0530

    declared attributes

---
+++ src/com/furniture/FurnitureItem.java
@@ -0,0 +1,10 @@
+package com.furniture;
+
+public class FurnitureItem {
+    private int furnitureCode;
+    private  String furnitureType;
+    private String gradeOfFurniture;
+    private String furnitureUsage;
+    private double price;
+
+}
