d87725a4d4c9493d2ba3ee86b056d282c11d370a
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-16 16:10:38 +0530

    completed the task

---
+++ src/com/furniture/FurnitureImpl.java
@@ -0,0 +1,14 @@
+package com.furniture;
+import com.furniture.FurnitureItem;
+public class FurnitureImpl {
+    public static void main(String[] args) {
+        FurnitureItem furnitureItem1 = new FurnitureItem();
+        furnitureItem1.setFurnitureCode(101);
+        furnitureItem1.setFurnitureType("table");
+        furnitureItem1.setGradeOfFurniture("grade1");
+        furnitureItem1.setFurnitureUsage("outdoor");
+        furnitureItem1.setPrice(10);
+
+        System.out.println("Discounted price is : $" + furnitureItem1.calculateDiscount());
+    }
+}
